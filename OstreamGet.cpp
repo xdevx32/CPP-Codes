3379d647e8c73e1ad32917778ba88f342e3de627
xdevx32 <angelkukushev333@gmail.com>
2019-08-27 14:24:15 +0300

    Adds ostream get member function few usage cases

---
+++ OstreamGet.cpp
@@ -0,0 +1,20 @@
+//
+// Created by aced3v on 8/27/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str1[10], str2[10];
+
+    // Usage without delimeter
+     cin.get(str1, 10).get(str2, 10);
+
+    // Usage with delimeter
+    //cin.get(str1, 10, '.').get(str2, 10, '.');
+
+    cout << "str1:" << str1 << "\nstr2:" << str2 << "\n";
+}
