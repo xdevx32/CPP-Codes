d92f634f00b853e5cf3e3236f82e5006a2fcfb64
xdevx32 <angelkukushev333@gmail.com>
2019-08-29 09:50:48 +0300

    Task1 completed

--- Task_1.cpp
+++ Task_1.cpp
@@ -7,6 +7,10 @@
 
 using namespace std;
 
+// Task : Write "Hello World!" to a file and then close the file.
+// Then open the same file and read and print in the console
+// the "Hello World!" using charachter arrays.
+
 int main()
 {
     ofstream f1;
@@ -23,7 +27,13 @@ int main()
         return 1;
     } else {
         char str[13];
-        file2 >> str;
+        // Both get and getline work in this case.
+        // We want to print the Hello World! string in
+        // Charachter array. But when just doing
+        // file >> str , it stops at whitespace.
+        // That's why we should use the get or getline
+        // function.
+        file2.get(str, 13);
         cout << str;
     }
 
